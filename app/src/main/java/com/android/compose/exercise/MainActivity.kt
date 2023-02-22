d95cfcc533311a06737f60f6f539dc65b6344ba2
Kai <tingnapianhai@gmail.com>
2023-02-23 00:18:35 +0100

    Create project with Empty-Compose-Activity

---
+++ app/src/main/java/com/android/compose/exercise/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.android.compose.exercise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.compose.exercise.ui.theme.AndroidComposeExerciseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidComposeExerciseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidComposeExerciseTheme {
+        Greeting("Android")
+    }
+}
